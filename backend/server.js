5b0ff4086270bdbbeb15e09cbb6f1b62355c1818
flaviurosca <flaviuroscaivanciuc@gmail.com>
2025-05-22 18:07:35 +0300

    update cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://product-store.flaviurosca.com"],
+  }),
+);
 
 app.use(express.json());
 
